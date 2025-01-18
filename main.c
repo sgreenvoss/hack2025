c017aa07201bdd8b979540d33a18656b9609f65c
sgreenvoss <sgreenvoss@gmail.com>
2025-01-17 18:03:30 -0800

    Initializing project based on GLFW-CMake-starter

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    if (!glfwInit()) {
+        printf("Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+    if (!window) {
+        printf("Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
