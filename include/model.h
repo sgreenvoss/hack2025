82387c83f584adf043461e7261efcea04428fa96
dhaddad-bit <dhaddad@uoregon.edu>
2025-01-18 15:38:26 -0800

    model initialized

---
+++ include/model.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define MIN_MAX_Z 10
+#define MIN_MAX_Z 10
+#define MIN_MAX_X 10
+
+#ifndef RES
+#define RES 100
+#endif
+
+
+// Vertex Struct
+typedef struct Vertex {
+    int x;
+    int y;
+    int z;
+} v;
+
---
+++ src/model.c
@@ -0,0 +1,33 @@
+#define YSTART -10
+#define YEND 10
+#define XSTART -10
+#define XEND 10
+#define GRID_RES 0.1
+#include <math.h>
+
+
+
+
+
+
+v * GenerateGrid()
+{
+    v * Grid = (v *)malloc(sizeof(v) )
+}
+//void ** GeneratePoints(xStart, xEnd, xRes, yStart, yEnd, yRes, void *
+
+for (int i=0; i<GRID_PTS/10; i++)
+{
+    for (int j=0; j<GRID_PTS/10; j++)
+    {
+        
+    }
+}
+
+void generateEllipticParaboloid(float **vertices, unsigned int **indices, 
+                                int segments_x, int segments_y, 
+                                float max_x, float max_y, 
+                                int *vertexCount, int *indexCount);
+int main(){
+
+}
