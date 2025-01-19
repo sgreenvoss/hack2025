0c37c6a2a55425c7e132cd55b26a027ade5afb0a
caarrp <junebirdsfly@gmail.com>
2025-01-18 18:43:34 -0800

    adding model to main

--- src/dofunctionstuff.c
+++ src/dofunctionstuff.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-#include <model.h>
+#include "model.h"
+#include <math.h>
+
+#define PI 3.1415926
+
+
 
 //in this file I want to test writing points of a function
 
@@ -8,20 +13,24 @@ float* Circle(float radius,float x_dif,float y_dif,float z_dif) {
 	//my center will be determined by the difference in each axis
 	//thus: no difference -> (0,0,0) y_diff = 2 -> (0,2,0)
 
+    float angle_step = 2 * PI / RES;  // Angle step in radians
+
+    printf("x\ty\n");  // Header for the output
+
+    for (int i = -radius; i < radius + RES; i++) {
+        float theta = i * angle_step;  // Current angle in radians
+        float x = radius * cos(theta);  // x-coordinate
+        float y = radius * sin(theta);
 
 	
 }
 
-float* Pyramid() {
+//float* Pyramid() {
 
-}
-
-float*
+//}
 
 
-
-int main() {
+//int main() {
 	//this is a test function that I want to use to write math to veritices
-	funcToCircle(0.5, 0.0, 0.0, 0.0);
 
-}
+//}
