61e3b9def3c1810be6b9dfdf13d4f24b7c835706
dhaddad-bit <dhaddad@uoregon.edu>
2025-01-18 18:21:31 -0800

    finished eliptic paraboloid function

--- src/model.c
+++ src/model.c
@@ -10,23 +10,70 @@
 
 
 
-v * GenerateGrid()
+float * GenerateGrid()
 {
     //MALLOC #of vertices in Grid 
-    v * Grid = (v *)malloc(sizeof(v) * ((2*MAX_X)*(2*MAX_Y)/RES));
-    v * index = Grid;
-    for (float i=-MAX_X; i <MAX_X; i+=RES)
+    float * Grid = (float *)malloc(sizeof(float) * ((2*MAX_X)*(2*MAX_Y)/RES)*3);
+    float * index = Grid;
+    for (float i=-MAX_X; i < MAX_X+RES; i+=RES)
     {
-        for (float j=-MAX_Y; j < MAX_Y; j+= RES)
+        for (float j=-MAX_Y; j < MAX_Y+RES; j+= RES)
         {
-            *index = v(i,j,0);
+            *index = i;
+            index++;
+            *index = j;
+            index++;
+            *index = 0; //Z coord
             index++;
         }
     }
+    return Grid;
+}
+
+void applyParaboloid(float** gridptr, int a, int b, int c)
+{
+    /*
+    Applies the z/c = (x^2 / a^2) + (y^2 / b^2)*/
+    float *grid = *gridptr;
+    for(int i=0; i<401; i++) { //FIXME
+        float x = *grid;
+        grid++;
+        float y = *grid;
+        grid++;
+
+        *grid = c * ((pow(x, 2)*pow(a, 2)) * (pow(y, 2)*pow(b, 2)));
+        grid++;
+    }
+}
+
+void normalize(float **gridptr)
+{
+    
 }
 //void ** GeneratePoints(xStart, xEnd, xRes, yStart, yEnd, yRes, void *
 
 int main()
 {
-    v vertex = GenerateGrid();
+    float* tmp = GenerateGrid();
+    applyParaboloid(&tmp, 1, 4, 4);
+
+    for (int i=0; i<=20; i++)
+    {
+        for (int j=0; j<=20; j++)
+        {
+            // printf("vertex(%.2f, ", *tmp);
+            // tmp++;
+            // printf("%.2f, ", *tmp);
+            // tmp++;
+            // printf("%.2f) ", *tmp);
+            // tmp++;
+            printf("(%.2f,", *tmp);
+            tmp++;
+            printf("%.2f,", *tmp);
+            tmp++;
+            printf("%.2f) ", *tmp);
+            tmp++;
+        }
+        printf("\n");
+    }
 }
--- src/model.h
+++ src/model.h
@@ -3,12 +3,12 @@
 #include <math.h>
 
 
-#define MAX_Z 10
-#define MAX_Y 10
-#define MAX_X 10
+#define MAX_Z 1
+#define MAX_Y 1
+#define MAX_X 1
 
 #ifndef RES
-#define RES 100
+#define RES .1
 #endif
 
 
